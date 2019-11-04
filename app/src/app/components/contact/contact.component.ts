95e700a1fb161d2ca0e684e7379153de85c8c21c
Willem Houm <willem.houm@gmail.com>
2019-11-04 22:28:23 +0000

    init form

--- app/src/app/components/contact/contact.component.ts
+++ app/src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.initForm()
   }
 
+  initForm() {
+    this.contactForm = this.formBuilder.group({
+      lastName: ['', [Validators.required, Validators.maxLength(30), Validators.minLength(2)]],
+      firstName: ['', [Validators.required, Validators.maxLength(30), Validators.minLength(2)]],
+      userEmail: ['', [Validators.required, Validators.email, Validators.maxLength(100), Validators.minLength(2)]],
+      emailMessage: ['', [Validators.required, Validators.maxLength(50), Validators.minLength(2)]]
+    })
+  }
+
+  onSubmit() {
+    const contactFormValue = this.contactForm.value;
+    console.log(contactFormValue);
+  }
 }
